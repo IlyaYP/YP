8d5499d2935e7ccc4572f64123583cb4b0ceca46
Ilya Suprun <pytok@yandex.ru>
2021-12-20 16:13:21 +0300

    Changes to be committed: 	new file:   go.mod 	new file:   main.go 	new file:   main_test.go

---
+++ sp1/2/http-testing/2/main_test.go
@@ -0,0 +1,46 @@
+/*
+Во втором примере покрыть тестами нужно функцию User.FullName(). 
+Для этого также скопируйте код себе в IDE и создайте файл main_test.go.
+*/
+package main
+
+import (
+	"testing"
+)
+
+func TestFullName(t *testing.T) {
+
+	tests := []struct { // добавился слайс тестов
+        name   string
+        value User
+        want string
+    }{
+        {
+            name:   "simple test #1", // описывается каждый тест
+            value: User{
+				FirstName: "Misha",
+				LastName:  "Popov",
+			},      // значения, которые будет принимать функция
+            want:   "Misha Popov",                // ожидаемое значение
+        },
+        {
+            name:   "simple test #2", // описывается каждый тест
+            value: User{
+				FirstName: "Ilya",
+				LastName:  "Suprun",
+			},      // значения, которые будет принимать функция
+            want:   "Ilya Suprun",                // ожидаемое значение
+        },
+	}
+
+
+	for _, tt := range tests { // цикл по всем тестам
+        t.Run(tt.name, func(t *testing.T) {
+            if val := tt.value.FullName(); val != tt.want {
+                t.Errorf("Abs expected to be %v; got %v", tt.want, val)
+            }
+        })
+    }
+
+
+}
