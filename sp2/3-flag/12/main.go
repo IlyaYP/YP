4121003a77ee019f64b7c3157610d4a830be2063
Ilya Suprun <pytok@yandex.ru>
2022-01-10 03:07:31 +0300

    new file:   main modified:   main.go

--- sp2/3-flag/12/main.go
+++ sp2/3-flag/12/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 var version = "0.0.1"
 
 func main() {
+	// допишите код
+	var oldUsage = flag.Usage
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s version:%s\n", os.Args[0], version)
+		oldUsage()
+	}
 	flag.Parse()
 }
